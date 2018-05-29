6b3c6bb97947060951f7f8141b50f7dedca34fa3
guq9618 <guq9618@naver.com>
2018-05-29 14:59:24 +0900

    Add files via upload

---
+++ fdrive/interface.py
@@ -0,0 +1,404 @@
+# -*- coding: cp949 -*-
+import clickable
+import sys
+import os
+import datetime
+from functools import partial
+from PyQt4 import QtCore, QtGui, uic
+import web
+import jj
+
+
+form_class = uic.loadUiType("interface.ui")[0]
+
+
+
+
+class MyWindowClass(QtGui.QMainWindow, form_class):
+
+    base_dir=os.getcwd()
+    #os.chdir(base_dir)
+    video_name=""
+    detail_flag=0
+    del_image_detail=[]
+    auto_search_flag=0
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        
+        self.nh_btn.clicked.connect(self.nh_btn_clicked)
+        self.jjd_btn.clicked.connect(self.jjd_btn_clicked)
+        self.log_btn.clicked.connect(self.log_btn_clicked)
+        self.ys_btn.clicked.connect(self.ys_btn_clicked)
+        self.del_btn.clicked.connect(self.del_btn_clicked)
+        self.search_btn.clicked.connect(self.search_btn_clicked)
+        self.yt_btn.clicked.connect(self.yt_btn_clicked)
+        
+        pixmap=QtGui.QPixmap("Logo.png")
+        self.detail_image_lab1.setPixmap(pixmap)
+        self.detail_image_lab1.setScaledContents(True)
+
+        self.clo_all.stateChanged.connect(self.checked_all)
+
+        
+
+
+    def load_data(self):
+        os.chdir("F:\\feature_data")
+        a=os.getcwd()
+        b=os.listdir(a)
+        
+        
+        data_alllist=[]
+        for i in range(len(b)):
+            data=open(b[i],"r")
+            data_line=data.readline().split()
+            data_list=[]
+
+            while data_line!=[]:
+                data_list.append(data_line)
+                data_line=data.readline().split()
+            data.close()
+
+            if i==0:
+                data_alllist=data_list
+            else:
+                data_alllist=data_alllist+data_list
+
+        os.chdir(self.base_dir)
+        return data_alllist
+
+        
+    def nh_btn_clicked(self):
+        web.webcam_control()
+
+
+    def yt_btn_clicked(self):
+        videofile_name=QtGui.QFileDialog.getOpenFileName()
+        jj.webcam_control(videofile_name)
+        
+    def jjd_btn_clicked(self):
+        os.chdir("F:\\video_data")
+        a=os.getcwd()
+        b=os.listdir(a)
+        
+        self.scro.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.GridLayout = QtGui.QGridLayout(self.scrollAreaWidgetContents)        
+        self.scro.setWidget(self.scrollAreaWidgetContents)
+
+        self.la=[]
+
+        if self.auto_search_flag==0:
+            if b==[]:
+                notice=QtGui.QMessageBox.warning(self,'Warning',u"저장되어있는 영상이 없습니다")
+        for i in range(len(b)):
+            videotime=b[i][0:19]
+            videotime=videotime[0:4]+u"년 "+ videotime[5:7]+u"월"+videotime[8:10]+u"일 "+videotime[11:13]+u"시"+videotime[14:16]+u"분"+videotime[17:19] + u"초"
+            self.la.append(QtGui.QPushButton(videotime))
+            self.GridLayout.addWidget(self.la[i])
+            clickable.clickable(self.la[i]).connect(lambda x=i: self.show_detail_video(x,b))
+        self.auto_search_flag=0
+        os.chdir(self.base_dir)
+        
+        
+    def log_btn_clicked(self):
+        log_data=open("Log.txt","r")
+        all_log_data=log_data.read()
+        log_data.close()
+
+        self.scro.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.GridLayout = QtGui.QGridLayout(self.scrollAreaWidgetContents)        
+        self.scro.setWidget(self.scrollAreaWidgetContents)
+
+        self.GridLayout.addWidget(QtGui.QLabel(all_log_data))
+
+        
+        
+    def ys_btn_clicked(self):
+        if self.video_name=="":
+            notice=QtGui.QMessageBox.warning(self,'Warning',u"선택된 영상이 없습니다")
+            
+        else:
+            os.chdir("F:\\video_data")
+            os.system(self.video_name)
+            os.chdir(self.base_dir)
+        
+    
+
+    def search_btn_clicked(self):
+        self.search_data()
+
+    def search_data(self):
+        self.video_name=""
+        self.scro.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.gridLayout = QtGui.QGridLayout(self.scrollAreaWidgetContents)        
+        self.scro.setWidget(self.scrollAreaWidgetContents)
+        
+        data_alllist=self.load_data()
+        result_list=[]
+        
+        t_to=self.time_to.dateTime()
+        t_to_string=t_to.toString("yyyy_MM_dd_hh_mm_ss")
+        t_from=self.time_from.dateTime()
+        t_from_string=t_from.toString("yyyy_MM_dd_hh_mm_ss")
+        color1=self.color_1.currentText()
+        color2=self.color_2.currentText()
+        color3=self.color_3.currentText()
+
+        dress=[]
+
+        if self.clo_shirts.isChecked():
+            dress.append('shirts')
+        if self.clo_cardigan.isChecked():
+            dress.append('cardigan')
+        if self.clo_short_tee.isChecked():
+            dress.append('short_tee')
+        if self.clo_suit.isChecked():
+            dress.append('suit')
+        if self.clo_coat.isChecked():
+            dress.append('coat')
+        if self.clo_training.isChecked():
+            dress.append('training')
+        if self.clo_military.isChecked():
+            dress.append('military')
+        if self.clo_vest.isChecked():
+            dress.append('vest')
+        if self.clo_dress.isChecked():
+            dress.append('dress')
+        if self.clo_jacket.isChecked():
+            dress.append('jacket')
+        if self.clo_hoodie.isChecked():
+            dress.append('hoodie')
+        if self.clo_skirt.isChecked():
+            dress.append('skirt')
+        
+        myTime_to = datetime.datetime.strptime(str(t_to_string), '%Y_%m_%d_%H_%M_%S')
+        myTime_from = datetime.datetime.strptime(str(t_from_string), '%Y_%m_%d_%H_%M_%S')
+        
+        check=[]
+        if color1 !='None':
+            check.append(color1)
+        if color2 !='None':
+            check.append(color2)
+        if color3 !='None':
+            check.append(color3)
+
+        
+        for d in data_alllist:
+            flag=True
+
+            dateANDtime = d[0][34:53]
+            myTime = datetime.datetime.strptime(dateANDtime, '%Y_%m_%d_%H_%M_%S')
+
+            if myTime_to > myTime or myTime > myTime_from:   ## check time
+                flag=False
+
+            for c in check:        ## check color
+                if c not in d:
+                    flag=False
+
+            if d[1] not in dress:      ## check clothes
+                flag=False
+        
+            
+
+            if flag:
+                result_list.append(d)
+        
+        if len(result_list)==0:
+            if self.auto_search_flag==0:
+                notice=QtGui.QMessageBox.warning(self,'Message',u"검색 결과가 존재하지 않습니다")
+        else:
+            count=0
+            self.la=[]
+
+        
+            for i in range(int((len(result_list)+2)/3)):
+                for j in range(3):
+                    if count==len(result_list):
+                        break
+                    os.chdir("F:\\")
+                    pixmap=QtGui.QPixmap(str(result_list[count][0]))
+                    self.la.append(QtGui.QLabel())
+                    pixmap=pixmap.scaledToWidth(150)
+                    self.la[count].setPixmap(pixmap)
+                    self.la[count].setScaledContents(True)
+                    self.gridLayout.addWidget(self.la[count], i, j)
+                    clickable.clickable(self.la[count]).connect(lambda x=count: self.show_detail_image(x,result_list[x]))
+                    count=count+1
+        
+            if count < 7:
+                for i in range(int(count/3),3):
+                    for j in range(3):
+                        if i==count/3:
+                            if j > (count%3-1):
+                                self.la.append(QtGui.QLabel())
+                                self.la[count].setScaledContents(True)
+                                self.gridLayout.addWidget(self.la[count], i, j)
+                        elif i != count/3:
+                            self.la.append(QtGui.QLabel())
+                            self.la[count].setScaledContents(True)
+                            self.gridLayout.addWidget(self.la[count], i, j)
+        self.auto_search_flag=0
+
+    def checked_all(self):
+        x=self.clo_all.isChecked()
+        self.clo_shirts.setChecked(x)
+        self.clo_cardigan.setChecked(x)
+        self.clo_short_tee.setChecked(x)
+        self.clo_suit.setChecked(x)
+        self.clo_coat.setChecked(x)
+        self.clo_training.setChecked(x)
+        self.clo_military.setChecked(x)
+        self.clo_vest.setChecked(x)
+        self.clo_dress.setChecked(x)
+        self.clo_jacket.setChecked(x)
+        self.clo_hoodie.setChecked(x)
+        self.clo_skirt.setChecked(x)
+        
+    def del_btn_clicked(self):
+        if self.detail_flag==0:
+            notice=QtGui.QMessageBox.warning(self,'Warning',u"삭제할 대상이 없습니다")
+        elif self.detail_flag==1:
+            result = QtGui.QMessageBox.question(self, 'Message', u"저장된 이미지와 특징데이터를 삭제하시겠습니까?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+            if result==QtGui.QMessageBox.Yes:
+                self.auto_search_flag=1
+                self.del_image()
+                self.detail_flag=0
+                self.clear_detail()
+                notice=QtGui.QMessageBox.information(self,'Message',u"삭제가 완료되었습니다")
+            else:
+                pass
+        elif self.detail_flag==2:
+            result = QtGui.QMessageBox.question(self, 'Message', u"저장된 영상과 관련된 데이터를 모두 삭제하시겠습니까?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+            if result==QtGui.QMessageBox.Yes:
+                self.auto_search_flag=1
+                self.del_video()
+                self.detail_flag=0
+                self.video_name=""
+                self.clear_detail()
+                notice=QtGui.QMessageBox.information(self,'Message',u"삭제가 완료되었습니다")
+            else:
+                pass
+
+    def del_video(self):
+        os.chdir("F:\\video_data")
+        os.system("del "+self.video_name)
+
+        data_locate=self.video_name[0:19]
+
+        os.chdir("F:\\feature_data")
+        os.system("del "+data_locate+".txt")
+
+        os.chdir("F:\\img_data")
+        os.system("rmdir /s /q "+data_locate)
+        os.chdir("F:\\video_preview")
+        os.system("del "+data_locate+".jpg")
+
+
+        os.chdir(self.base_dir)
+        now=self.now_time()
+        log_data=open("Log.txt","a")
+        log_data.writelines(now+" Delete_Video   "+self.video_name+" \n")
+        log_data.close()
+
+        self.jjd_btn_clicked()
+
+        
+    def del_image(self):
+        os.chdir("F:")
+        del_data=''
+        for i in self.del_image_detail:
+            del_data=del_data+i+" "
+        del_data=del_data[0:len(del_data)-1]
+        data_locate=del_data[11:30]
+        data_file=open('feature_data/'+data_locate+".txt",'r')
+        data=data_file.read()
+        data_file.close()
+        data_del=data.replace(del_data+"\n","")
+        data_file=open('feature_data/'+data_locate+".txt",'w')
+        data_file.write(data_del)
+        data_file.close()        
+
+
+        
+        os.chdir("F:\\img_data\\"+data_locate)
+
+        os.system("del "+del_data[31:60])
+        
+
+        os.chdir(self.base_dir)
+        
+        now=self.now_time()
+        
+        log_data=open("Log.txt","a")
+        log_data.writelines(now+" Delete_Image   "+del_data+" \n")
+        log_data.close()
+        
+        
+        self.search_data()
+        
+    def show_detail_image(self,index,result_list):
+        self.detail_flag=1
+        os.chdir("F:")
+        pixmap=QtGui.QPixmap(str(result_list[0]))
+        self.detail_image_lab1.setPixmap(pixmap)
+        self.video_name=result_list[0][11:30]+".avi"
+        videotime=result_list[0][11:30]
+        mantime=result_list[0][34:53]
+        mancolor=result_list[2:len(result_list)]
+        self.detail_videotime.setText(u"영상 촬영시간 : " + videotime[0:4]+u"년 "+ videotime[5:7]+u"월"+videotime[8:10]+u"일 "+videotime[11:13]+":"+videotime[14:16]+":"+videotime[17:19])
+        self.detail_mantime.setText(u"인물 등장시간 : " + mantime[0:4]+u"년 "+ mantime[5:7]+u"월"+mantime[8:10]+u"일 "+mantime[11:13]+":"+ mantime[14:16]+":"+ mantime[17:19])
+        self.detail_clo.setText(u"등장인물 의상정보 : " + result_list[1])
+        str_mancolor=''
+        for i in mancolor:
+            if i==mancolor[0]:
+                str_mancolor=i
+            else:
+                str_mancolor=str_mancolor+" "+i
+        self.detail_color.setText(u"등장인물 색상정보 : " + str_mancolor)
+        self.del_image_detail=result_list
+        os.chdir(self.base_dir)
+        
+    def show_detail_video(self,index,video_list):
+        self.detail_flag=2
+        os.chdir("F:\\")
+        self.video_name=video_list[index]
+        videotime=video_list[index]
+        pixmap=QtGui.QPixmap("video_preview/"+videotime[0:19]+".jpg")
+        self.detail_image_lab1.setPixmap(pixmap)
+        self.detail_videotime.setText(u"영상 촬영시간 : " + videotime[0:4]+u"년 "+ videotime[5:7]+u"월"+videotime[8:10]+u"일 "+videotime[11:13]+":"+videotime[14:16]+":"+videotime[17:19])
+        self.detail_mantime.setText(" ")
+        self.detail_clo.setText(" ")
+        self.detail_color.setText(" ")
+        os.chdir(self.base_dir)
+
+    def now_time(self):
+        year = datetime.datetime.today().year
+        month = datetime.datetime.today().month
+        day = datetime.datetime.today().day
+        hour = datetime.datetime.today().hour
+        minute = datetime.datetime.today().minute
+        second = datetime.datetime.today().second
+
+        now=str(year)+"/ "+str(month)+"/"+str(day)+" "+str(hour)+":"+str(minute)+":"+str(second)
+        return now
+
+    def clear_detail(self):
+        os.chdir(self.base_dir)
+        pixmap=QtGui.QPixmap("Logo.png")
+        self.detail_image_lab1.setPixmap(pixmap)
+        self.detail_image_lab1.setScaledContents(True)
+
+        self.detail_videotime.setText(u"영상 촬영시간 : ")
+        self.detail_mantime.setText(u"인물 등장시간 : ")
+        self.detail_clo.setText(u"등장인물 의상정보 : ")
+        self.detail_color.setText(u"등장인물 색상정보 : ")
+                                  
+app = QtGui.QApplication(sys.argv)   
+myWindow = MyWindowClass()
+myWindow.show()
+app.exec_()                          
---
+++ final/web.py
@@ -0,0 +1,158 @@
+import datetime
+import cv2
+import time
+import os
+from config import *
+from feature_function import *
+
+def reinit_tracker(frame, hog, _type="hog"):
+    tracker = cv2.MultiTracker_create()
+    if _type == "hog":
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4,4), padding=(0,0), scale=1.18)
+    for rect in rects:
+        tracker.add(cv2.TrackerKCF_create(), frame, tuple(rect))
+
+    return tracker
+
+def predict_with_write_detections(img,rects,nowstr,index,f,thickness=1):
+        res_data=[]
+        for   [x,y,w,h] in rects:
+                #print x,y,w,h
+                #crop img create        
+                pad_w,pad_h = int(0.15*w-1),int(0.05*h-1)
+                x_min,x_max,y_min,y_max=x+pad_w,x+w-pad_w,y+pad_h,y+h-pad_h
+                crop_img=img[y_min:y_max ,x_min:x_max]
+
+                #save
+                imwrite_str=img_data_path+nowstr[0]+'/img'+nowstr[1]+'_%02d'%(index)+'.jpg'
+                tmp='a'
+                while os.path.isfile(imwrite_str) == True:
+                        imwrite_str=imwrite_str[:-6]+tmp+imwrite_str[-5:]
+                        tmp= chr(ord(tmp)+1)
+                cv2.imwrite(imwrite_str,crop_img)
+                #feature data   
+                colors=image_color_cluster(crop_img)
+                res=predict_img(imwrite_str)[0]
+                score=predict_img(imwrite_str)[1]
+                res_data.append([res,colors])
+                res_write_str=imwrite_str+' '+res+' '+' '.join(colors)+'\n'
+                f.write(res_write_str)
+
+        #print 'shape',(res_data[0])
+        return res_data[0],score
+
+def webcam_control():
+        filename=datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+        current_datetime=datetime.datetime.now()
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        #video init
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        #output = cv2.VideoWriter(video_data_path+filename+".avi",fourcc,20.0,(1920,1080))
+        output = cv2.VideoWriter(video_data_path+filename+".avi",fourcc,20.0,(640,360))
+        #webcam = cv2.VideoCapture('test/rotate.avi')
+        #webcam = cv2.VideoCapture('test/test.avi')
+        webcam = cv2.VideoCapture(0)
+        webcam.set(1, 60)
+
+        hog = cv2.HOGDescriptor()
+        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+        #img init
+	
+        if not os.path.isdir(img_data_path+filename):
+                os.mkdir(img_data_path+filename)
+
+	#feature init
+        f=open(feature_data_path+filename+'.txt','a')
+
+        #track
+        tracker=cv2.MultiTracker_create()
+        needHogVerif=0
+        allBoxes=[]
+        speedBoxes=[]
+        Boxes_feature=[]
+        focalLength=0
+        HOG_VERIF_TRESH=24
+        KNOWN_DISTANCE = 24.0
+        KNOWN_WIDTH = 11.0
+        MAX_DISTANCE = 10
+        isNew=True
+        isNew2=True
+        start=True
+        
+        while webcam.isOpened():
+                #declare
+                 
+                start_time=time.time()	
+                current_datetime=datetime.datetime.now()
+                current_datetime_str=current_datetime.strftime('%Y/%m/%d  %H:%M:%S')
+                current_datetime_str2=current_datetime.strftime('%Y_%m_%d_%H_%M_%S')
+
+                 #read
+                ret,frame=webcam.read()
+                if start:
+                   cv2.imwrite('./video_preview/'+filename+".jpg",frame)
+	
+
+                #print frame.shape
+                #copy_frame=frame.copy()
+                copy_frame=cv2.resize(frame,(640,360))#,interpolation=cv2.INTER_AREA)
+                
+                #cv2.imshow('origin',copy_frame)
+                #process
+                ok,boxes=tracker.update(copy_frame)	
+                nBox=0
+                
+                if not ok or boxes is () or needHogVerif > HOG_VERIF_TRESH:
+			#cv2.imshow('origin',copy_frame)
+			#cv2.imshow('cframe',copy_frame)
+                        needHogVerif=0
+                        tracker = reinit_tracker(copy_frame,hog)
+                        isNew=True
+                        isNew2=True
+
+                        speedBoxes=[]
+                        Boxes_feature=[]
+                        allBoxes=[]
+			#fps=str(int(1/(time.time()-start_time)))
+			#cv2.putText(copy_frame,'fps:'+fps,(50,50),font,1,(255,255,255),2,cv2.LINE_AA)
+			#cv2.putText(copy_frame,current_datetime_str,(420,30), font, 0.5,(255,255,255),2,8)
+
+			#cv2.imshow('cframe',copy_frame)
+			#output.write(copy_frame)
+
+                if isNew2==False:
+                        realboxes=[]
+                        for i , box in enumerate(boxes):
+                                if isNew:
+                                        allBoxes.append([box])
+                                        Boxes_feature.append(predict_with_write_detections(copy_frame,[[int(box[0]),int(box[1]),int(box[2]),int(box[3])]],[filename,current_datetime_str2],i,f))
+                                else:
+                                        allBoxes[nBox].append(box)
+				
+                        for i , box in enumerate(boxes):
+                                pad_w,pad_h = int(0.15*box[2]-1) , int(0.05*box[3]-1) 
+                                p1 = (int(box[0])+pad_w, int(box[1])+pad_h)
+                                p2 = (int(box[2])+int(box[0])-pad_w, int(box[3])+int(box[1])-pad_h)
+                                cv2.rectangle(copy_frame, p1, p2, (255,255,255), 2)
+                                cv2.putText(copy_frame,str(Boxes_feature[i][0][0])+':'+str(int(Boxes_feature[i][1]*100))+'%',(int(box[0]), int(box[1])+5), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,255,255),1, cv2.LINE_AA)
+        
+                                nBox+=1
+	
+                        isNew=False
+                isNew2=False
+                needHogVerif+=1
+	
+			#draw
+		#print time.time()-start_time
+		#fps=str(int(1/(time.time()-start_time)))
+		#cv2.putText(copy_frame,'fps:'+fps,(50,50),font,1,(255,255,255),2,cv2.LINE_AA)
+                cv2.putText(copy_frame,current_datetime_str,(420,30), font, 0.5,(255,255,255),2,8)
+
+                cv2.imshow('cframe',copy_frame)
+                output.write(copy_frame)
+		#ui
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                        break
